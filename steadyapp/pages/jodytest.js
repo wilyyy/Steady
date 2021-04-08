123a07368a593537649cce4e9217e202bc7df598
Jody Prinsloo <jprinsloo@my.bcit.ca>
2021-04-08 00:30:18 -0700

    Finished Component

---
+++ steadyapp/pages/jodytest.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Combo from '../comps/HeadingAndParagraph'
+
+export default function Test(){
+  return <Combo/>
+}
