5bd00126dc56bbfd28d879ef6373748b11181aa3
IbjyotGill <ibjyot17@gmail.com>
2021-04-08 00:58:53 -0700

    ibbySteady!

---
+++ steadyapp/pages/test.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+import Continue from '../comps/Continue';
+
+const ContainerMain = styled.div`
+`
+
+export default function Test() {
+  return <ContainerMain>
+      <Head>
+      </Head>
+      <Continue />
+    </ContainerMain>
+}
