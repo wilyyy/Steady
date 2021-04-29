887a152a190c065f4aa4eaf2385466d5005bc6f5
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-04-28 19:41:54 -0700

    Modified title screen

--- steadyapp/comps/Logo/index.js
+++ steadyapp/comps/Logo/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import styled from'styled-components';
+import {useRouter} from 'next/router';
 
 const LogoPicture = styled.img`
     width: ${props=>props.width};
     height: auto;
     object-fit: contain;
     z-index: 1;
-    cursor: pointer;
+    cursor: pointer;  
 `
 
 const Logo = ({
     width="500px",
-    src="/logo.png"
+    src="/logo.png",
+    routeTo = "/"
 }) => {
-    return <LogoPicture width={width} src={src} />
+    const router = useRouter();
+    return <LogoPicture width={width} src={src} onClick={()=>router.push(routeTo)} />
 }
 
 export default Logo;
--- steadyapp/pages/index.js
+++ steadyapp/pages/index.js
@@ -6,6 +6,17 @@ import Button from '../comps/Button';
 import Background from '../comps/BackgroundImage';
 
 const TitleContainer = styled.div`
+    background-color: #FA956B;
+    overflow: hidden;
+    @keyframes fadein {
+        0% {
+            opacity: 0%;
+        }
+        100% {
+            opacity: 100%;
+        }
+    }
+
     .column {
         display: flex;
         flex-direction: column;
@@ -20,24 +31,73 @@ const TitleContainer = styled.div`
         padding: 20px;
     }
 
-    background-color: #FA956B;
-    overflow: hidden;
+    .fade {
+        animation-name: fadein;
+        animation-duration: 1s;
+        z-index: 5;
+    }
 
-    
+    .fade2 {
+        animation-name: fadein;
+        animation-duration: 2s;
+        animation-delay: 1s;
+        z-index: 5;
+        opacity: 0%;
+        animation-fill-mode: forwards;
+        position: relative;
+        bottom: 40px;
+    }
+
+    .fade3 {
+        animation-name: fadein;
+        animation-duration: 1s;
+        animation-delay: 4s;
+        z-index: 5;
+        opacity: 0%;
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes backnforth {
+        0% {
+            transform: translate(-300px, 0px);
+        }
+        100% {
+            transform: translate(-260px, 0px);
+        }
+    }
+
+    .waves {
+        animation-name: backnforth;
+        animation-duration: 2s;
+        animation-iteration-count: infinite;
+        animation-timing-function: ease-in-out;
+        animation-direction: alternate;
+        position: absolute;
+        bottom: 0px;
+    }
 `
 
 export default function Title(){
+    
     return <TitleContainer>
         <Head>
             <title>Steady Homepage</title>
         </Head>
         <div className="column">
-            <Logo width="300px" src="/ver_variant3.png" />
-            <Subheader quotetext="Let Steady help you find out some causes and remedies to help you get back on track to a happy life" undertext="" fsize="2rem" />
-            <Button buttontext="Let's Begin" bgcolor= "#3A6FF9" textcolor="#FFF4DD" routeTo="/about" />
-            <Background source="/waves.svg" width="1000px" bottom= "-850px" filter= "invert(10%) sepia(51%) saturate(6943%) hue-rotate(246deg) brightness(82%) contrast(106%)" />
-            <Background source="/waves.svg" width="1000px" bottom= "-950px" filter="invert(15%) sepia(19%) saturate(7472%) hue-rotate(234deg) brightness(76%) contrast(110%)" />
-            <Background source="/waves.svg" width="1000px" bottom= "-1050px" filter="invert(6%) sepia(64%) saturate(2564%) hue-rotate(241deg) brightness(92%) contrast(94%)" />
+            <div className="fade">
+                <Logo width="300px" src="/ver_variant3.png" />
+            </div>
+            <div className="fade2">
+                <Subheader quotetext="Let Steady help you find out some causes and remedies to help you get back on track to a happy life" undertext="" fsize="2rem" />
+            </div>
+            <div className="fade3">
+                <Button buttontext="Let's Begin" bgcolor= "#3A6FF9" textcolor="#FFF4DD" routeTo="/about" />
+            </div>
+            <div className="waves">
+                <Background source="/waves.svg" width="1000px" bottom= "-850px" filter= "invert(10%) sepia(51%) saturate(6943%) hue-rotate(246deg) brightness(82%) contrast(106%)" />
+                <Background source="/waves.svg" width="1000px" bottom= "-950px" filter="invert(15%) sepia(19%) saturate(7472%) hue-rotate(234deg) brightness(76%) contrast(110%)" />
+                <Background source="/waves.svg" width="1000px" bottom= "-1050px" filter="invert(6%) sepia(64%) saturate(2564%) hue-rotate(241deg) brightness(92%) contrast(94%)" />
+            </div>
         </div>
     </TitleContainer>
 }
