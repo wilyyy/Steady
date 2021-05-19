a13345a968a13a8639e9e3cbc82a742c8e6cebe5
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-05-19 00:20:46 -0700

    added session storage to subquestions

--- steadyapp/pages/sleep.js
+++ steadyapp/pages/sleep.js
@@ -10,6 +10,7 @@ import Continue from '../comps/Continue';
 // import {IoIosArrowDropleft, IoIosArrowDropright} from 'react-icons/io';
 // import { IconContext } from "react-icons";
 import React, {useState} from 'react';
+import { useRouter } from 'next/router';
 
 const TitleContainer = styled.div`
     background-color: #616BD1;
@@ -30,33 +31,46 @@ const TitleContainer = styled.div`
     }
 `
 
+var result = null;
+
 export default function Title(){
+
+    const router = useRouter();
     //const [srcOne, setSrcOne] = useState("../../meal_icon_bw.png");
     const [picstate1, setPicState1] = useState(false);
     const [picstate2, setPicState2] = useState(false);
     const [picstate3, setPicstate3] = useState(false);
     const [continuestate, setContinueState] = useState(false);
 
-    const HandleClick1 = () =>{
+    const HandleClick1 = (text) =>{
         setPicState1(true);
         setPicState2(false);
         setPicstate3(false);
         setContinueState(true);
+        result = text;
 
     }
 
-    const HandleClick2 = () =>{
+    const HandleClick2 = (text) =>{
         setPicState1(false);
         setPicState2(true);
         setPicstate3(false);
         setContinueState(true);
+        result = text;
     }
 
-    const HandleClick3 = () =>{
+    const HandleClick3 = (text) =>{
         setPicState1(false);
         setPicState2(false);
         setPicstate3(true);
         setContinueState(true);
+        result = text;
+    }
+
+    const HandleEnd = () => {
+        alert(result);
+        sessionStorage.setItem("sleepresult", result);
+        router.push("/mainquestion");
     }
 
     return <TitleContainer>
@@ -66,18 +80,19 @@ export default function Title(){
         <div className="column">
             <TopBar />
             <Combo src="/sleep.png" subtitle1="" title1="How much did you sleep last night?" width="150px"/>
-            <BoxyButton display="none" layout="center" position="static" text="<7 Hours" onClick={HandleClick1}
+            <BoxyButton display="none" layout="center" position="static" text="<7 Hours" onClick={()=>HandleClick1("TooLittle")}
             bgcolor={picstate1 ? "#F57C33" : "#E8E0CD"}
             textcolor = {picstate1 ? "white" : "#131521"} />
-            <BoxyButton display="none" layout="center" position="static" text="7-9 Hours" onClick={HandleClick2}
+            <BoxyButton display="none" layout="center" position="static" text="7-9 Hours" onClick={()=>HandleClick2("Perfect")}
             bgcolor={picstate2 ? "#F57C33" : "#E8E0CD"}
             textcolor = {picstate2 ? "white" : "#131521"} />
-            <BoxyButton display="none" layout="center" position="static" text="9+ Hours" onClick={HandleClick3}
+            <BoxyButton display="none" layout="center" position="static" text="9+ Hours" onClick={()=>HandleClick3("TooMuch")}
             bgcolor={picstate3 ? "#F57C33" : "#E8E0CD"}
             textcolor = {picstate3 ? "white" : "#131521"} />
             <Continue 
             bgColor={continuestate ? "#00BF08" : "rgba(232, 224, 205, 0.5)"}
-            DivOpacity={continuestate ? "100%" : "50%"} />
+            DivOpacity={continuestate ? "100%" : "50%"}
+            onClick={HandleEnd} />
             <NavBar />
         </div>
     </TitleContainer>
